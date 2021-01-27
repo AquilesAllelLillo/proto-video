ddf3f7e4fa5c0918f266c3d03db1e208f3c0018f
unknown <aquilesallellillo@gmail.com>
2021-01-27 10:29:21 -0300

    Implementando Redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -23,7 +23,11 @@ const Home = ({ mylist, trends, originals }) => {
         <Categories title="Mi lista">
           <Carousel>
             {mylist.map(item =>
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem 
+                key={item.id} 
+                {...item} 
+                isList
+              />
             )}
           </Carousel>
         </Categories>
